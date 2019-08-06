f73db68930738005f57ad18d97132a23bb8b42bb
Michael Finlay <finlayma84@MacBook-Air.local>
2019-08-06 01:04:42 -0500

    further styling details

---
+++ routes/testimonials.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+var Testimonial = require("../models/testimonials");
+
+//index
+
+//new
+
+//create
+
+//show (do I need this?)
+
+//edit
+
+//update
+
+//destroy
+
